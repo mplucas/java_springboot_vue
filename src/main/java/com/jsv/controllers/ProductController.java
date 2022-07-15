d6569eb9dd6780764eda15f8b036ac3d9dbc997b
mplucas <lucas-m.p@hotmail.com>
2022-07-15 01:10:15 -0300

    - Java app port to 3000; - Started request from vue to Java using axios.

--- src/main/java/com/jsv/controllers/ProductController.java
+++ src/main/java/com/jsv/controllers/ProductController.java
@@ -2,12 +2,14 @@ package com.jsv.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jsv.models.Product;
 import com.jsv.repository.ProductRepository;
 
+@CrossOrigin
 @RestController
 public class ProductController {
 	
--- src/main/resources/static/SPA/src/components/products/Products.vue
+++ src/main/resources/static/SPA/src/components/products/Products.vue
@@ -1,8 +1,36 @@
 <template>
-    <h2 class="centralizado">Produtos</h2>
+    <div>
+        <h2 class="centralizado">Produtos</h2>
+        <ul class="list-group">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">A fourth item</li>
+            <li class="list-group-item">And a fifth one</li>
+        </ul>
+    </div>
 </template>
 
-<script></script>
+<script>
+import http from '../../http/index.ts'
+
+export default {
+    data() {
+        return {
+            products: []
+        }
+    },
+
+    created() {
+        http
+            .get('products')
+            .then(response => {
+                this.products = response.data
+                console.log(this.products)
+            })
+    }
+}
+</script>
 
 <style scoped>
 .centralizado {
