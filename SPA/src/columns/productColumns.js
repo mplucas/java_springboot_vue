fab2e3668625053263d15b5ef7b3b4018ad2d6ae
mplucas <lucas-m.p@hotmail.com>
2022-07-18 23:18:25 -0300

    Product dropdown type

--- SPA/src/columns/productColumns.js
+++ SPA/src/columns/productColumns.js
@@ -1,7 +1,7 @@
 const columns = [
     { name: 'productID', display: 'Código', isKey: true },
     { name: 'description', display: 'Descrição' },
-    { name: 'type', display: 'Tipo' },
+    { name: 'type', display: 'Tipo', type: 'dropDown', options: ['Eletrônico', 'Eletrodoméstico', 'Móvel'] },
     { name: 'buyPrice', display: 'Preço de compra' },
     { name: 'stockQuantity', display: 'Quantidade' }
 ]
